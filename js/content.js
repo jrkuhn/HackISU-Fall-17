fd9ea5049b37fb46c1600ae5ef1c13ce46ba2feb
Justin Kuhn <jrkuhn@iastate.edu>
2017-10-21 02:34:15 -0500

    Added code notes

--- js/content.js
+++ js/content.js
@@ -2,16 +2,26 @@
 alert(document.title);
 
 var images = [];
+
+//documents.images is all images in the page
 for(var i = 0; i < document.images.length; i++){
+  //create new image in array index
   images[i] = new Image();
+
+  //add image to array index
   images.push(document.images[i]);
 
+  //title to image and print its title in the console
   images[i].src = document.images[i].src;
   console.log(images[i].src);
 }
 
+//finds all videos
 var videos = document.querySelectorAll('a[href$=".mp4"]');
 
+if(videos.length > 0) {
+  console.log(videos[i].src);
+}
 
 
 // var imgElements = document.getElementsByTagName("img");
