957dd23c4094027ad8a460473e49103c8b3bb3d8
Justin Kuhn <jrkuhn@iastate.edu>
2017-10-21 00:38:02 -0500

    read all imgs in page

--- js/content.js
+++ js/content.js
@@ -1,2 +1,7 @@
 //alert("Hello, This is FlashOff");
 //alert(document.title);
+
+var mediaElements = document.getElementsByTagName("img");
+for(i = 0 i < mediaElements.length, i++) {
+  alert(mediaElements);
+}
