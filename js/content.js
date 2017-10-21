7a05b058b2a74673aa1a0f23391b5a600be6c0d7
Justin Kuhn <jrkuhn@iastate.edu>
2017-10-21 01:08:57 -0500

    find all images in page

--- js/content.js
+++ js/content.js
@@ -1,7 +1,24 @@
 //alert("Hello, This is FlashOff");
-//alert(document.title);
+alert(document.title);
 
-var mediaElements = document.getElementsByTagName("img");
-for(i = 0 i < mediaElements.length, i++) {
-  alert(mediaElements);
+var images = [];
+for(var i = 0; i < document.images.length; i++){
+  images.push(document.images[i].src);
+  alert(images[i]);
 }
+
+// var imgElements = document.getElementsByTagName("img");
+// var videoElements = documet.getElementsbyTagName("video");
+
+
+// if(imgElements.length > 0) {
+//   for(i = 0 i < imgElements.length, i++) {
+//     alert(imgElements[i].src);
+//   }
+// }
+//
+// if(videoElements.length > 0) {
+//   for(i = 0 i < videoElements.length, i++) {
+//     alert(videoElements[i].src);
+//   }
+// }
