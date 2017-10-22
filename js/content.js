c84dc7c394d46164889659b328d87d58145d88a1
Justin Kuhn <jrkuhn@iastate.edu>
2017-10-21 20:51:26 -0500

    added image saving

--- js/content.js
+++ js/content.js
@@ -4,6 +4,7 @@ alert(document.title);
 var images = [];
 var gifs = document.querySelectorAll('img[src$=".gif"]');
 var gifArray = [];
+var gifArrayScreenshots = [];
 
 var superGifs = [];
 
@@ -17,33 +18,48 @@ for(var i = 0; i < gifs.length; i++){
   //title to image and print its title in the console
   images[i].src = gifs[i].src;
   console.log(images[i].src);
-  document.body.appendChild(gifs[i]);
+  //document.body.appendChild(gifs[i]);
+
+//gifFrames
+  gifFrames({ url: gifs[i].src, frames: 'all', outputType: 'canvas' }).then(function (frameData) {
+    gifArrayScreenshots[i] = new Array();
+    //prints/saves all frames in the gif as DOM images.
+    for(j=0; j < frameData.length; j++){
+      gifArrayScreenshots[i][j] = frameData[j].getImage();
+      document.body.appendChild(frameData[j].getImage());
+    }
+    document.body.appendChild(gifArrayScreenshots[i][0]);
+    console.log(frameData.length);
+  }).catch(console.error.bind(console));
+}
+
+
+
+
 
 //doesn't work now
   //var ex = document.getElementById("example");
-   superGifs[i] = new SuperGif({gif: gifs[i]});
-
-   superGifs[i].load(function (err) {
-        if (err) {
-            console.error(err);
-        } else {
-            console.log('loaded '+i);
-        }
-    });
-   console.log(superGifs[i].get_auto_play());
-   console.log(superGifs[i].get_current_frame());
-   superGifs[i].pause(function(){console.log('paused ' + i)});
-
-  if(superGifs[i].get_length() <= 1){
-    console.log(i + gifs[i].src  + ' static: ' + superGifs[i].get_length() + ' frames');
-  }
-  else {
-    console.log("image" + i + "moving");
-  }
-  console.log(superGifs[i].get_current_frame());
+  //  superGifs[i] = new SuperGif({gif: gifs[i]});
+  //
+  //  superGifs[i].load(function (err) {
+  //       if (err) {
+  //           console.error(err);
+  //       } else {
+  //           console.log('loaded '+i);
+  //       }
+  //   });
+  //  console.log(superGifs[i].get_auto_play());
+  //  console.log(superGifs[i].get_current_frame());
+  //  superGifs[i].pause(function(){console.log('paused ' + i)});
+  //
+  // if(superGifs[i].get_length() <= 1){
+  //   console.log(i + gifs[i].src  + ' static: ' + superGifs[i].get_length() + ' frames');
+  // }
+  // else {
+  //   console.log("image" + i + "moving");
+  // }
+  // console.log(superGifs[i].get_current_frame());
 //
-}
-
 
 
 //finds all videos
